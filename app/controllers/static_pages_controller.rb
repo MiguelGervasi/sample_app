f98e2910c3221b0a7ec74948dc1ed2028e4edad0
Miguel Gervasi <Miguel.Gervasi@gmail.com>
2013-10-11 21:04:36 -0400

    Add user following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,8 @@ class StaticPagesController < ApplicationController
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
-  end  
+  end
+
   def help
   end
 
@@ -13,4 +14,5 @@ class StaticPagesController < ApplicationController
   
   def contact
   end
+
 end
